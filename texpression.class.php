78fa4d335058bede246b7d5dcc3d6759a2ea05c8
diaspedro7 <gerhardtgaroto@gmail.com>
2024-09-25 14:05:16 -0300

    criacao da classroom expressao
    Classe que gerencia expressões..

---
+++ texpression.class.php
@@ -0,0 +1,14 @@
+<?php
+
+//Classe TExpression
+//*Classe abstrata para gerenciar expressões
+
+abstract class TExpression {
+//Operadores logicos
+
+const AND_OPERATOR = 'AND';
+const OR_OPERATOR = 'OR';
+
+
+}
+?>
